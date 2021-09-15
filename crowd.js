fb5352c37e3cea208432b2967d33becd9753c7ae
Yehia Dawood <75096933+Yoha30@users.noreply.github.com>
2021-09-15 19:35:14 +0200

    ...

--- crowd.js
+++ crowd.js
@@ -5,6 +5,8 @@ var bookmarked = document.createTextNode("ed");
 let backProject = document.getElementById('back-project');
 let backgroundImage = document.getElementById('background-image');
 let aboutSection = document.getElementById('about-this');
+let logo = document.getElementById('logo');
+
 function bookMark() {
     if (greenBookmark.classList.contains('hidden')) {
         grayBookmark.classList.add('hidden');
@@ -18,17 +20,21 @@ function bookMark() {
         bookmarkColor.removeChild(bookmarked);
     }
 }
-function backThisProject (){
-    if(backProject.classList.contains('hidden')){
+
+function backThisProject() {
+    if (backProject.classList.contains('hidden')) {
         backProject.classList.remove('hidden');
         backgroundImage.classList.add('opacity-50');
         aboutSection.classList.add('opacity-50');
+        logo.classList.add('hidden');
     }
 }
-function closeModal (){
-    if (backgroundImage.classList.contains('opacity-50')){
+
+function closeModal() {
+    if (backgroundImage.classList.contains('opacity-50')) {
         backProject.classList.add('hidden');
         backgroundImage.classList.remove('opacity-50');
         aboutSection.classList.remove('opacity-50');
+        logo.classList.remove('hidden');
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -27,14 +27,17 @@ module.exports = {
             width: {
                 '4/7': '53.5%',
                 '42': '10.7rem',
+                '68': '17rem',
+                "75": '19rem',
+                '78': '21rem',
                 '84': '23rem',
-                '97':'37rem',
+                '97': '37rem',
                 '98': '39rem',
                 '100': '201vw',
             },
             height: {
                 '100': '40vw',
-
+                '89': '22rem',
             },
             margin: {
                 '84': '21.5rem',
@@ -45,12 +48,18 @@ module.exports = {
                 '82': '20.5rem',
                 '84': '22rem',
                 '86': '24rem',
+                '85': '23rem',
+                '88': '37.5rem',
                 '96': '26rem',
                 '98': '29rem',
+                '98.5': '24.75rem',
                 '99': '32rem',
                 '99.5': '34.5rem',
+                '99.6': '35.25rem',
+                '99.7': '36.75rem',
                 '100': '38rem',
                 '110': '46rem',
+                '115':'47.5rem',
                 '120': '49rem',
                 '150': '54rem',
                 '175': '62rem',
@@ -63,8 +72,8 @@ module.exports = {
                 'radio': '#3CB4ACFF',
                 'check': '#147B74FF',
             },
-            divideColor :{
-              'darkgreen':'#147B74FF',
+            divideColor: {
+                'darkgreen': '#147B74FF',
             },
 
             screens: {
